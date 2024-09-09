6659360c8d51f7812dd3f57d76858abd1bc72e51
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-09 19:24:58 +0530

    #18 :- Adds @tanstack/react-query library.

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient(); 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
