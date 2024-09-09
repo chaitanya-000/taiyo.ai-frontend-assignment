4dd79c3b5cae6427a54186b87384ae76fea62e12
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-09 22:11:17 +0530

    #19 :- Creates Loading component.

---
+++ src/molecules/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center '>
+      <h1 className='tracking-tight font-bold animate-ping'>
+        Gathering Data..
+      </h1>
+    </div>
+  );
+}
