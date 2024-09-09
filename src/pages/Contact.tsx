9b31cd59aa2aa46bbd968bd3643bde3e919f0b89
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-09 17:40:52 +0530

    #13 :- Adds the ContactsList component in the Contact Page.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Contact_CreateContactModal from "../organisms/Contact_CreateContactModal";
+import ContactsList from "../organisms/Contact_ContactList";
 
 export default function Contact() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -9,7 +10,7 @@ export default function Contact() {
   };
 
   return (
-    <div className="w-full flex flex-col items-center">
+    <div className="w-full flex flex-col h-2/3 bg-red-300s justify-between items-center">
       <button
         onClick={toggleModal}
         className=" text-white bg-green-700 hover:bg-green-800 hover:font-bold focus:ring-4 focus:outline-none focus:ring-slate-600 font-medium rounded-lg text-lg px-5 py-3.5 text-center "
@@ -17,6 +18,7 @@ export default function Contact() {
       >
         Create Contact
       </button>
+      <ContactsList />
 
       {isModalOpen && (
         <Contact_CreateContactModal setIsModalOpen={setIsModalOpen} />
