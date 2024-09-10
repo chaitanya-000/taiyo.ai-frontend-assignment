8d96ba5a23284af2824c36d62c17ca7c633f02c4
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-10 10:04:40 +0530

    #26 :- Adds the GET requests for countires & Statistics. Adds env variable for base url.

--- src/apiRequests.ts
+++ src/apiRequests.ts
@@ -2,10 +2,24 @@ import { CovidData } from "./types";
 
 export const fetchCovidData = async (): Promise<CovidData> => {
   const response = await fetch(
-    "https://disease.sh/v3/covid-19/historical/all?lastdays=all"
+    `${process.env.REACT_APP_BASE_URL}/historical/all?lastdays=all`
   );
   if (!response.ok) {
     throw new Error("There was some trouble gathering the Data.");
   }
   return response.json();
+};
+
+export const fetchCountryData = async () => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/countries`);
+  if (!response.ok) throw new Error("Failed to fetch country data.");
+  const json = await response.json();
+  return json;
+};
+
+export const fetchWorldCovidStats = async () => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/all`);
+  const json = await response.json();
+  if (!response.ok) throw new Error("Failed to fetch world data.");
+  return json;
 };
