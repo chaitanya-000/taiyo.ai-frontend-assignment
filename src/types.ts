e67fe9ca24ce36617cab6f5ab9c131382f1b987e
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-10 10:08:06 +0530

    #27 :- Adds CountryData type in src/types.

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,13 @@ export interface CovidData {
   deaths: Record<string, number>;
   recovered: Record<string, number>;
 }
+export interface CountryData {
+  country: string;
+  countryInfo: {
+    lat: number;
+    long: number;
+  };
+  active: number;
+  deaths: number;
+  recovered: number;
+}
