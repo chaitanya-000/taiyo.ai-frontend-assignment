08f1a61246e0e8dc63433c60dedf00c9f050bb2c
chaitanya-joshi-99 <Chaitanya99j@gmail.com>
2024-09-10 10:37:10 +0530

    #32 :- Adds the Statistics organism in the Charts_And_Maps page.

--- src/pages/Charts_And_Maps.tsx
+++ src/pages/Charts_And_Maps.tsx
@@ -1,10 +1,12 @@
 import ChartsAndMaps_Charts from "../organisms/ChartsAndMaps_Charts";
 import Map from "../organisms/Map";
+import Statistics from "../organisms/Statistics";
 export default function Charts_And_Maps() {
   return (
     <>
       <ChartsAndMaps_Charts />
       <Map />
+      <Statistics />
     </>
   );
 }
